c2fc56aa5a9facfa5c1270640839753b19d91e3d
NamerPRO <mail@namerpro.me>
2022-10-19 21:53:46 +0300

    Значительное лучшение реализации и исправление мелких ошибок

--- Laboratornaya3/task6_1/cstring.h
+++ Laboratornaya3/task6_1/cstring.h
@@ -5,31 +5,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <time.h>
+#include <ctype.h>
 
 #include "cconstants.h"
-
-#define STANDART_STRING_SIZE 255
 
 typedef struct string {
   char* str;
   unsigned long int size;
 } string;
 
-void create_string(string* str, char* value);
+int create_string(string* str, char* value);
+int create_all_strings(unsigned long int count, ...);
 int create_empty_string(string* str);
+int create_all_empty_strings(unsigned long int count, ...);
 int read_string(FILE* stream, string* str, int (*cmp)(int x));
+int read_all_strings(FILE* stream, int (*cmp)(int x), unsigned long int count, ...);
 unsigned long int get_string_size(string* str);
-void print_string(FILE* stream, string* str);
+void print_string(FILE* stream, char* pattern, ...);
 void free_string(string* str);
+void free_all_strings(unsigned long int count, ...);
 char* get_raw_string(string* str);
 int standart_string_comporator(string* str1, string* str2);
-int string_compare(string* str1, string* str2, int (*cmp)(string* str1, string* str2));
+int string_compare(char* pattern, int (*cmp)(string* str1, string* str2), string* str1, ...);
 int create_string_duplicate(string* str, string* cpy_str);
-int string_concat(unsigned long int count, string* out_str, ...);
+int string_concat(char* pattern, string* out_str, ...);
 int string_copy(string* from, string* to);
-void remove_data_from_string(string* str, unsigned long int left_bound, unsigned long int right_bound);
-char* str(char* value);
+//Добавить string_cut, вырезающий часть строки
+//Добавить string_trim, что сжимает строку до ее размера
+void string_gnaw(string* str, unsigned long int left_bound, unsigned long int right_bound);
+char* str(char* value, int* execute_status);
 int reset_string(string* str);
 char* get_trimmed_string(string* str, int* execute_status);
+char standart_random_character(unsigned long int position);
+int get_random_string(string* str, unsigned long int size, char (*rnd_char)(unsigned long int position));
+int contains_only(string* str, int (*in_range)(char symbol));
+int standart_string_range(char symbol);
+int standart_alphabet_comporator(string* str1, string* str2);
+void to_upper_case(string* str);
+void to_lower_case(string* str);
+int not(int status_code);
+int to_string(string* str_str, char* pattern, ...);
+int string_assign(string* str_str, char* str_new);
+void string_reverse(string* str_str, unsigned long int lhs, unsigned long int rhs);
 
 #endif
--- Laboratornaya3/task6_1/main.c
+++ Laboratornaya3/task6_1/main.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 #include "cconstants.h"
 #include "cstring.h"
 
-int toenter(int x) {
-  if ((char)x == '\n') {
-    return 1;
-  }
-  return 0;
-}
+int main(int argc, char* argv[]) {
+  string a, b;
+  create_all_empty_strings(2, &a, &b);
 
-int main() {
-  string dinamic_str;
-  create_string(&dinamic_str, str("nation"));
-  print_string(stdout, &dinamic_str);
+  get_random_string(&a, 10, standart_random_character);
+  get_random_string(&b, 5, standart_random_character);
 
-  remove_data_from_string(&dinamic_str, 1, 3);
-  print_string(stdout, &dinamic_str);
-
-  free_string(&dinamic_str);
+  print_string(stdout, "str1 = %s\nstr2 = %s\n", &a, &b);
+  free_all_strings(2, &a, &b);
 
   return 0;
 }
